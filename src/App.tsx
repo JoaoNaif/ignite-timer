2d8770adb4beb848f7c0e981ead4eac2520151e1
João Victor Naif De Abreu <joaonaif@localhost.localdomain>
2024-09-19 02:31:36 -0300

    updated eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { Button } from "./components/Button"
+import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
 
-import { defaultTheme } from "./styles/themes/default"
-import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button/>
-
-      <GlobalStyle/>
+      ;
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button />
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
 
-export default App;
+export default App
