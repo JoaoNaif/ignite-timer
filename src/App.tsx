c137eb64fa90f8c2896c45958326e1fbb0bc1de6
João Victor Naif De Abreu <joaonaif@localhost.localdomain>
2024-09-20 01:30:49 -0300

    create Home and History

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
-
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      ;
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,59 @@
+import { Play } from 'phosphor-react'
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="task_suggestions"
+            placeholder="Dê um nome para seu projeto"
+          />
+
+          <datalist id="task_suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 1" />
+            <option value="Projeto 1" />
+            <option value="Corinthians" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
